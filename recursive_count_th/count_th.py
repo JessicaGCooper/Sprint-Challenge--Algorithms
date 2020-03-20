a2146a6c33efc084627ef296ba0e3928d478dc19
Jessica Cooper <jessicagcooper@gmail.com>
2020-03-20 16:54:29 -0500

    further changes to recursive function

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,23 +6,44 @@ Your function must utilize recursion. It cannot contain any loops.
 word = 'thingith'
 
 def count_th(word):
-    
+    count = 0
     # result = word.find('th')
     # return result
     # TBC
-    count = 0
-    i = -2
-    print(len(word) - 1)
-    if i == -1:
+    
+    if len(word) < 2:
         return count
-    elif i < len(word):
-        i = word.find('th')
-        print(i)
-        if i >= 0:
+    else:
+        if word[0] == 't' and word[1] == 'h':
             count += 1
-            word = word[i+2:]
-            print(word)
+            word = word[2:]
+        else:
+            word = word[1:]
         
     count_th(word)
 
 print(count_th(word))
+
+# word = 'thingith'
+
+# def count_th(word, count=0, i=-2):
+    
+#     # result = word.find('th')
+#     # return result
+#     # TBC
+#     print(len(word) - 1)
+#     if len(word) < 2:
+#         return count
+#     elif i == -1:
+#         return count
+#     elif i < len(word):
+#         i = word.find('th')
+#         print(i)
+#         if i >= 0:
+#             count += 1
+#             word = word[i+2:]
+#             print(word)
+        
+#     count_th(word, count, i)
+
+# print(count_th(word))
